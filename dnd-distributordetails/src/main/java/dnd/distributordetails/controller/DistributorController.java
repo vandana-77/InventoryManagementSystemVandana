0c1208447dfac79a23c7d1f8aaf486dfc71322aa
HP <HP@DESKTOP-UES1JHE>
2020-09-23 08:26:55 +0530

    Distributor

---
+++ dnd-distributordetails/src/main/java/dnd/distributordetails/controller/DistributorController.java
@@ -0,0 +1,93 @@
+package dnd.distributordetails.controller;
+
+import java.util.List;
+import org.springframework.http.HttpHeaders;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dnd.distributordetails.exception.DistributorIdNotFoundException;
+
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import dnd.distributordetails.model.DistributorEntity;
+import dnd.distributordetails.service.DistributorServiceImpl;
+
+@RestController
+@RequestMapping("/distributor")
+public class DistributorController {
+
+	@Autowired
+	DistributorServiceImpl service;
+	
+	@GetMapping("/a")
+	public String getMessage() {
+		return "Welcome";
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<DistributorEntity> addDistributorDetails(@RequestBody DistributorEntity details) {
+		 service.addDistributorDetails(details);
+		 return new ResponseEntity<>(details,HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteDistributorDetails(@PathVariable("id") int distributorId) {
+		service.deleteDistributorDetails(distributorId);
+		return new ResponseEntity<String>("Deleted Requested Id",HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/id/{distributorId}")
+	//@HystrixCommand(fallbackMethod = "distributorFallback")
+	public DistributorEntity getDistributorDetails(@PathVariable int distributorId) {
+		return service.getDistributorDetails(distributorId);
+	}
+	
+	@GetMapping("/all")
+	public ResponseEntity< List<DistributorEntity> > getAllDistributorDetails(){
+		List<DistributorEntity> list=service.getAllDistributorDetails();
+		return new ResponseEntity<List<DistributorEntity>>(list,HttpStatus.OK);
+}
+	
+@PutMapping("/updateDistributorId")
+/*
+ * public DistributorEntity updateDistributor(@RequestBody DistributorEntity
+ * distributor) { return service.updateDistributor(distributor); }
+ */
+
+
+  public ResponseEntity<String> updateDistributorId(@RequestBody DistributorEntity distributor)
+  {
+	DistributorEntity e = service.updateDistributor(distributor); if (e == null)
+	{
+    throw new DistributorIdNotFoundException("Update Operation Unsuccessful,Provided Id does not exist"); 
+    } 
+  else
+  {
+   return new ResponseEntity<String>("DistributorEntity updated successfully", new HttpHeaders(), HttpStatus.OK); 
+   } 
+  }
+  }
+  
+ 
+
+/*
+ * * @GetMapping("/RMdetails/{distributorId}") public DistributorEntity
+ * fetchRawMaterialDetails(Integer distributorId,String delivereyStatus) {
+ * return null;
+ * 
+ * }
+ * 
+ * public DistributorEntity distributorFallback(@PathVariable int distributorId)
+ * { return new DistributorEntity(101,"praveena","hyd", (long) 912344430); }
+ */
